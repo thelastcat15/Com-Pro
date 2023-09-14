27dea1a18375d600e495a51135b864db73b5de08
thelastcat15 <smart000firstfenix@gmail.com>
2023-09-15 00:40:41 +0700

    c

--- S1/Exam guide/W10/ข้าวหลามตัด.py
+++ S1/Exam guide/W10/ข้าวหลามตัด.py
@@ -1,12 +1,11 @@
-x = int(input())
+x = int(input()) + 1
 
 def create(center) :
-    list_line = [i for i in range(1,center)] + [i for i in range(center,0,-1)] # สร้างเป็น list
+    list_line = list(range(1,center)) + list(range(center,0,-1))
     return "|".join(map(str, list_line)) # connect
 
-
 for i in range(1,x) :
-    print("  "*(x-i) + create(i))
+    print("  "*(x-i) + create(i) + "|")
 
-for i in range(x,0,-1) :
-    print("  "*(x-i) + create(i))
+for i in range(x, 0, -1):
+    print("  "*(x-i) + create(i) + "|")
