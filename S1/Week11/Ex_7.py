669e1286d42a8f143af09e9d5b99cee3ea1682aa
thelastcat15 <smart000firstfenix@gmail.com>
2023-10-06 00:25:05 +0700

    edit_6_10_2023

--- S1/Week11/Ex_7.py
+++ S1/Week11/Ex_7.py
@@ -4,14 +4,6 @@ f = Tk()
 f.geometry("150x110")
 f.resizable(False,False)
 
-grades = "FFFFFDCBAAA"
-def cal():
-    current_value = recive.get()
-    if current_value < 0 :
-        display.set("E")
-    else :
-        display.set(grades[current_value//10])
-
 recive = IntVar()
 recive.set("")
 Entry(f, textvariable=recive).pack()
