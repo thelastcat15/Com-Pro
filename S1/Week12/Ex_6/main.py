d628c7d9b1fcaf8cf0438bd9e5b38e57cbeec2a3
thelastcat15 <smart000firstfenix@gmail.com>
2023-10-12 18:22:30 +0700

    fix2 12/10/2023

--- S1/Week12/Ex_6/main.py
+++ S1/Week12/Ex_6/main.py
@@ -7,21 +7,12 @@ from csv import *
 
 
 try :
-    data = open("data.csv",'r',encoding='utf-8')
-    f_txt = open("b.txt",'w',encoding='utf-8')
-    f_csv = open("b.csv",'w',encoding='utf-8')
+    f_txt = open("b.txt",'r',encoding='utf-8')
+    f_csv = open("b.csv",'r',encoding='utf-8')
 
-    writer_csv = writer(f_csv, lineterminator="\n")
+    print(f_txt.read())
+    print(list(reader(f_csv)))
 
-    data_list = list(reader(data))
-    for row in data_list :
-        txt = " ".join(row)
-        print(txt)
-        
-        writer_csv.writerow(row)
-        f_txt.write(txt+"\n")
-
-    data.close()
     f_txt.close()
     f_csv.close()
 except :
